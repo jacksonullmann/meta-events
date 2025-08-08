7f6a7d49a2ee91722e08265e5f3e56b12ec0043c
jacksonullmann <163340577+jacksonullmann@users.noreply.github.com>
2025-08-08 17:02:52 -0300

    Update view-oferta.js

--- api/view-oferta.js
+++ api/view-oferta.js
@@ -1,14 +1,17 @@
-import fetch from 'node-fetch';
-
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método não permitido' });
   }
 
-  const accessToken = 'EAAEZCxlkR04ABPFP9Xv0yCkQpNrSy52QmM9IWLS3vQkyghWasgisOYScXFJCMKjff2xZAUoo34YNwUEZBOCKuKE6V4JJbgSg5w0xt5HoYz7VAKU9BZAdE0oTbcp9l6XOHiB2DqMYp1TfA9jr9bZChj7CvJqOk8PFbBlTqOXw2hJuZBpMKSa0m2EXy0ZCdATMgZDZD';
+  const accessToken = process.env.FB_ACCESS_TOKEN;
+  const pixelId = '1237017037714103';
+
+  if (!accessToken) {
+    return res.status(500).json({ error: 'Token de acesso não configurado' });
+  }
 
   try {
-    const response = await fetch(`https://graph.facebook.com/v18.0/1237017037714103/events?access_token=${accessToken}`, {
+    const response = await fetch(`https://graph.facebook.com/v18.0/${pixelId}/events?access_token=${accessToken}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
