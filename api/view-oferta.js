167aeaca72c35d0b8b797dc6557468e74886abb7
jacksonullmann <163340577+jacksonullmann@users.noreply.github.com>
2025-08-08 16:56:28 -0300

    Update view-oferta.js

--- api/view-oferta.js
+++ api/view-oferta.js
@@ -1,10 +1,14 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Método não permitido' });
   }
 
+  const accessToken = 'EAAEZCxlkR04ABPFP9Xv0yCkQpNrSy52QmM9IWLS3vQkyghWasgisOYScXFJCMKjff2xZAUoo34YNwUEZBOCKuKE6V4JJbgSg5w0xt5HoYz7VAKU9BZAdE0oTbcp9l6XOHiB2DqMYp1TfA9jr9bZChj7CvJqOk8PFbBlTqOXw2hJuZBpMKSa0m2EXy0ZCdATMgZDZD';
+
   try {
-    const response = await fetch('https://graph.facebook.com/v18.0/1237017037714103/events?access_token=EAAEZCxlkR04ABPFP9Xv0yCkQpNrSy52QmM9IWLS3vQkyghWasgisOYScXFJCMKjff2xZAUoo34YNwUEZBOCKuKE6V4JJbgSg5w0xt5HoYz7VAKU9BZAdE0oTbcp9l6XOHiB2DqMYp1TfA9jr9bZChj7CvJqOk8PFbBlTqOXw2hJuZBpMKSa0m2EXy0ZCdATMgZDZD', {
+    const response = await fetch(`https://graph.facebook.com/v18.0/1237017037714103/events?access_token=${accessToken}`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -22,10 +26,14 @@ export default async function handler(req, res) {
     });
 
     const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(result.error?.message || 'Erro desconhecido');
+    }
+
     res.status(200).json({ status: 'Enviado com sucesso', result });
   } catch (error) {
-    console.error('Erro ao enviar evento:', error);
-    res.status(500).json({ error: 'Erro interno do servidor' });
+    console.error('Erro ao enviar evento:', error.message);
+    res.status(500).json({ error: error.message });
   }
 }
-
