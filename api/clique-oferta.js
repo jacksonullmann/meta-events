0773558aa117597605e1cf2136041fbe4304f212
jacksonullmann <163340577+jacksonullmann@users.noreply.github.com>
2025-08-09 01:31:09 -0300

    Update clique-oferta.js

--- api/clique-oferta.js
+++ api/clique-oferta.js
@@ -1,25 +1,27 @@
 export default async function handler(req, res) {
-  const accessToken = 'SEU_TOKEN';
-  const pixelId = 'SEU_PIXEL_ID';
-  const testEventCode = 'TEST99526';
+  const accessToken = process.env.ACCESS_TOKEN;
+  const pixelId = process.env.PIXEL_ID;
+  const TEST_EVENT_CODE = 'TEST734';
+  const testEventCode = process.env.NODE_ENV === 'production' ? null : TEST_EVENT_CODE;
+
+  const url = `https://graph.facebook.com/v18.0/${pixelId}/events?access_token=${accessToken}` +
+              (testEventCode ? `&test_event_code=${testEventCode}` : '');
 
   try {
-    const response = await globalThis.fetch(`https://graph.facebook.com/v18.0/${pixelId}/events?access_token=${accessToken}&test_event_code=${testEventCode}`, {
+    const response = await fetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        data: [
-          {
-            event_name: 'CliqueOferta',
-            event_time: Math.floor(Date.now() / 1000),
-            action_source: 'website',
-            event_source_url: 'https://celularpro.kpages.online/retratos',
-            user_data: {
-              client_ip_address: req.headers['x-forwarded-for'] || '0.0.0.0',
-              client_user_agent: req.headers['user-agent'] || ''
-            }
+        data: [{
+          event_name: 'CliqueOferta',
+          event_time: Math.floor(Date.now() / 1000),
+          action_source: 'website',
+          event_source_url: 'https://celularpro.kpages.online/retratos',
+          user_data: {
+            client_ip_address: req.headers['x-forwarded-for'] || '0.0.0.0',
+            client_user_agent: req.headers['user-agent'] || ''
           }
-        ]
+        }]
       })
     });
 
