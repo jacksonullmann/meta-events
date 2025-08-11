4028804d92bdaffa52c35f557d180af11b11a759
jacksonullmann <163340577+jacksonullmann@users.noreply.github.com>
2025-08-11 20:00:56 -0300

    Update scroll-inicio.js

--- api/scroll-inicio.js
+++ api/scroll-inicio.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   const url = `https://graph.facebook.com/v18.0/${pixelId}/events?access_token=${accessToken}`;
 
   const payload = {
-    test_event_code: 'TEST56515',
+    test_event_code: process.env.TEST_EVENT_CODE,
     data: [
       {
         event_name: 'ScrollInicio',
