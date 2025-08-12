a819460159d8c272e29de82fb3de9c0fc751dfc3
jacksonullmann <163340577+jacksonullmann@users.noreply.github.com>
2025-08-12 12:18:50 -0300

    Update evento-generico.js

--- api/evento-generico.js
+++ api/evento-generico.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-modo-evento');
+
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
